84a5877fc1958b0000f977c62414dd87d8f39550
zzzonked <yaroslavkuts@gmail.com>
2018-12-30 17:11:55 +0200

    Basic CRUD operations for product types were added.

--- app/Controllers/Http/TypeController.js
+++ app/Controllers/Http/TypeController.js
@@ -1,22 +1,48 @@
+const Type = use('App/Models/Type');
+
 class TypeController {
-  async get() {
-    return { endpoint: 'get' };
+  async get({ request }) {
+    const { type } = request.post();
+    return {
+      message: 'ok',
+      data: type
+    };
   }
 
   async list() {
-    return { endpoint: 'list' };
+    const types = await Type.all();
+    return {
+      message: 'ok',
+      data: types
+    };
   }
 
-  async create() {
-    return { endpoint: 'create' };
+  async store({ request }) {
+    const body = request.post();
+    const type = await Type.create(body);
+    return {
+      message: 'Product type was created',
+      data: type
+    };
   }
 
-  async update() {
-    return { endpoint: 'update' };
+  async update({ request }) {
+    const { type, attributes } = request.post();
+    type.attributes = attributes;
+    await type.save();
+    return {
+      message: 'Product type was updated',
+      data: type
+    };
   }
 
-  async delete() {
-    return { endpoint: 'delete' };
+  async delete({ request }) {
+    const { type } = request.post();
+    await type.delete();
+    return {
+      message: 'Product type was deleted',
+      data: { id: type.id }
+    };
   }
 }
 
