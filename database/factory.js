09cb7e52a405806488f85c06ad7a31f7c4ec6111
zzzonked <yaroslavkuts@gmail.com>
2018-12-31 11:24:47 +0200

    Factory blueprints were added.

--- database/factory.js
+++ database/factory.js
@@ -1,19 +1,33 @@
-/*
-|--------------------------------------------------------------------------
-| Factory
-|--------------------------------------------------------------------------
-|
-| Factories are used to define blueprints for database tables or Lucid
-| models. Later you can use these blueprints to seed your database
-| with dummy data.
-|
-*/
+const Factory = use('Factory');
+const Hash = use('Hash');
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+Factory.blueprint('App/Models/User', async (faker, index, data) => ({
+  firstname: faker.first(),
+  lastname: faker.last(),
+  email: faker.email(),
+  phone: faker.phone(),
+  role: data.role || 'user',
+  address: faker.address(),
+  password: await Hash.make(faker.password())
+}));
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/Type', (faker, index, data) => ({
+  title: data.title || faker.word(),
+  attributes: data.attributes || {
+    attr1: 'value1',
+    attr2: 'value2',
+    attr3: 'value3'
+  }
+}));
+
+Factory.blueprint('App/Models/Product', (faker, index, data) => ({
+  title: data.title || faker.word(),
+  price: data.price || faker.integer({ min: 1, max: 20000 }),
+  properties: data.properties || {
+    attr1: 'value1',
+    attr2: 'value2',
+    attr3: 'value3'
+  },
+  type_id: data.type_id || faker.integer({ min: 1, max: 1000 }),
+  user_id: data.user_id || faker.integer({ min: 1, max: 1000 })
+}));
