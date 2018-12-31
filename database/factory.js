11a5bcf748c1045e959a0c70bb25f98bbe2cbb20
zzzonked <yaroslavkuts@gmail.com>
2018-12-31 12:28:13 +0200

    Factory blueprints were refactored.

--- database/factory.js
+++ database/factory.js
@@ -1,14 +1,13 @@
 const Factory = use('Factory');
-const Hash = use('Hash');
 
 Factory.blueprint('App/Models/User', async (faker, index, data) => ({
-  firstname: faker.first(),
-  lastname: faker.last(),
-  email: faker.email(),
-  phone: faker.phone(),
+  firstname: data.firstname || faker.first(),
+  lastname: data.lastname || faker.last(),
+  email: data.email || faker.email(),
+  phone: data.phone || faker.phone(),
   role: data.role || 'user',
-  address: faker.address(),
-  password: await Hash.make(faker.password())
+  address: data.address || faker.address(),
+  password: data.password || faker.password()
 }));
 
 Factory.blueprint('App/Models/Type', (faker, index, data) => ({
@@ -24,9 +23,9 @@ Factory.blueprint('App/Models/Product', (faker, index, data) => ({
   title: data.title || faker.word(),
   price: data.price || faker.integer({ min: 1, max: 20000 }),
   properties: data.properties || {
-    attr1: 'value1',
-    attr2: 'value2',
-    attr3: 'value3'
+    prop1: 'value1',
+    prop2: 'value2',
+    prop3: 'value3'
   },
   type_id: data.type_id || faker.integer({ min: 1, max: 1000 }),
   user_id: data.user_id || faker.integer({ min: 1, max: 1000 })
