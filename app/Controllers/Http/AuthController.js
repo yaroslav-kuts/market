fa83dea287281c9b9afc9daa0b180844dc4bbcd0
zzzonked <yaroslavkuts@gmail.com>
2018-12-31 12:30:58 +0200

    Authenticate method was implemented.

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,11 @@
 class AuthController {
-  async authenticate() {
-    return { endpoint: 'authenticate' };
+  async authenticate({ request, auth }) {
+    const { email, password } = request.post();
+    const token = await auth.attempt(email, password);
+    return {
+      message: 'Authentication was successfully completed',
+      data: token
+    };
   }
 }
 
