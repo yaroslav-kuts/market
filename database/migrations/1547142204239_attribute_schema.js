a9f58616b0de435cd9371e5fd2f99f8c7fc6ee8a
zzzonked <yaroslavkuts@gmail.com>
2019-01-10 19:44:33 +0200

    Migration file for attribute model was created.

---
+++ database/migrations/1547142204239_attribute_schema.js
@@ -0,0 +1,22 @@
+const Schema = use('Schema');
+
+class AttributeSchema extends Schema {
+  up() {
+    this.create('attributes', table => {
+      table.increments();
+      table.string('name', 80).notNullable();
+      table
+        .integer('type_id')
+        .notNullable()
+        .unsigned();
+      table.timestamps();
+      table.foreign('type_id').references('types.id');
+    });
+  }
+
+  down() {
+    this.drop('attributes');
+  }
+}
+
+module.exports = AttributeSchema;
