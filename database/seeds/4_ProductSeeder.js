d5b0537232b9643a13129f93087703020f4817c4
zzzonked <yaroslavkuts@gmail.com>
2019-01-16 20:24:14 +0200

    Product seeder was refactored.

--- database/seeds/4_ProductSeeder.js
+++ database/seeds/4_ProductSeeder.js
@@ -5,19 +5,36 @@ const Attribute = use('App/Models/Attribute');
 
 class ProductSeeder {
   async run() {
+    const productTasks = [];
     const { rows: users } = await User.all();
     const { rows: types } = await Type.all();
     for (const { id: user_id } of users) {
       for (const { id: type_id } of types) {
-        const { id: product_id } = await Factory.model('App/Models/Product').create({ user_id, type_id });
-        const { rows: attributes } = await Attribute.query()
-          .where('type_id', '=', type_id)
-          .fetch();
-        for (const { id: attribute_id } of attributes) {
-          await Factory.get('product_attributes').create({ product_id, attribute_id });
-        }
+        productTasks.push(Factory.model('App/Models/Product').create({ user_id, type_id }));
       }
     }
+    const products = await Promise.all(productTasks);
+
+    const attributeTasks = [];
+    for (const { type_id } of products) {
+      attributeTasks.push(
+        Attribute.query()
+          .where('type_id', '=', type_id)
+          .fetch()
+      );
+    }
+    const attributes = await Promise.all(attributeTasks);
+
+    const productAttributes = attributes.reduce(
+      (acc, attrs, index) =>
+        acc.concat(attrs.rows.map(row => ({ product_id: products[index].id, attribute_id: row.id }))),
+      []
+    );
+    await Promise.all(
+      productAttributes.map(({ product_id, attribute_id }) =>
+        Factory.get('product_attributes').create({ product_id, attribute_id })
+      )
+    );
   }
 }
 
